6d44aaee9b218e61276730fe6e19ed482df1073f
chang0022 <chang.0022@163.com>
2018-07-13 23:32:18 +0800

    众筹合约出版

---
+++ contracts/Project.sol
@@ -0,0 +1,83 @@
+pragma solidity ^0.4.24;
+
+contract Project {
+    struct Payment {
+        string description;
+        uint amount;
+        address receiver;
+        bool completed;
+        address[] voters;
+    }
+
+    address public owner;        // 项目所有者
+    string public description;   // 项目介绍
+    uint public minInvest;       // 最小投资金额
+    uint public maxInvest;       // 最大投资金额
+    uint public goal;            // 融资上限
+    address[] public investors;  // 投资人列表
+    Payment[] public payments;   // 资金支出列表
+
+    constructor(string _description, uint _minInvest, uint _maxInvest, uint _goal) public {
+        owner = msg.sender;
+        description = _description;
+        minInvest = _minInvest;
+        maxInvest = _maxInvest;
+        goal = _goal;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minInvest);
+        require(msg.value <= maxInvest);
+        require(address(this).balance <= goal);
+
+        investors.push(msg.sender);
+    }
+
+    function createPayment(string _description, uint _amount, address _receiver) public {
+        Payment memory newPayment = Payment({
+            description: _description,
+            amount: _amount,
+            receiver: _receiver,
+            completed: false,
+            voters: new address[](0)
+        });
+
+        payments.push(newPayment);
+    }
+
+    function approvePayment(uint index) public {
+        Payment storage payment = payments[index];
+
+        // must be investor to vote
+        bool isInvestor = false;
+        for (uint i = 0; i < investors.length; i++) {
+            isInvestor = investors[i] == msg.sender;
+            if (isInvestor) {
+                break;
+            }
+        }
+        require(isInvestor);
+
+        // can not vote twice
+        bool hasVoted = false;
+        for (uint j = 0; j < payment.voters.length; j++) {
+            hasVoted = payment.voters[j] == msg.sender;
+            if (hasVoted) {
+                break;
+            }
+        }
+        require(!hasVoted);
+
+        payment.voters.push(msg.sender);
+    }
+
+    function doPayment(uint index) public {
+        Payment storage payment = payments[index];
+        
+        require(!payment.completed);
+        require(payment.voters.length > (investors.length / 2));
+
+        payment.receiver.transfer(payment.amount);
+        payment.completed = true;
+    }
+}
