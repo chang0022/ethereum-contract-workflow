13b4ff1cafcc28ae687eae657283951fdab43204
chang0022 <chang.0022@163.com>
2018-07-14 22:49:51 +0800

    添加编译、部署和自动化测试

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,10 @@
+const fs = require('fs-extra')
 const path = require('path')
 const Web3 = require('web3')
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
 // 1. 获取 bytecode
-const contractPath = path.resolve(__dirname, '../compiled/Car.json')
+const contractPath = path.resolve(__dirname, '../compiled/ProjectList.json')
 const { interface, bytecode } = require(contractPath)
 
 // 2. 配置 provider
@@ -17,14 +18,24 @@ const web3 = new Web3(provider)
 ;(async () => {
   // 4. 获取钱包里面的账户
   const accounts = await web3.eth.getAccounts()
-  console.log('部署合约的账户：', accounts[0])
+  console.log('合约部署账户:', accounts[0])
 
-  // 5. 创建合约实例并部署
-  console.time('Contract-deploy')
+  // 5. 创建合约实例并且部署
+  console.time('合约部署耗时')
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: `0x${bytecode}`, arguments: ['AUDI'] })
-    .send({ from: accounts[0], gas: '1000000' })
-  console.timeEnd('Contract-deploy')
+    .deploy({ data: `0x${bytecode}` })
+    .send({ from: accounts[0], gas: '5000000' })
+  console.timeEnd('合约部署耗时')
 
-  console.log('合约部署成功：', result.options.address)
+  const contractAddress = result.options.address
+
+  console.log('合约部署成功:', contractAddress)
+  console.log('合约查看地址:', `https://rinkeby.etherscan.io/address/${contractAddress}`)
+
+  // 6. 合约地址写入文件系统
+  const addressFile = path.resolve(__dirname, '../address.json')
+  fs.writeFileSync(addressFile, JSON.stringify(contractAddress))
+  console.log('地址写入成功:', addressFile)
+
+  process.exit()
 })()
